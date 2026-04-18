960e756404c2fa100f61cca5dcf365b83a8ba61c
karamlk <karam.alrifai24@gmail.com>
2026-04-18 23:48:27 +0300

    refactor: remove redundant category query and use relationship-based grouping

--- app/Http/Resources/FavoriteCollection.php
+++ app/Http/Resources/FavoriteCollection.php
@@ -3,22 +3,21 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
-use App\Models\Category;
 
 class FavoriteCollection extends ResourceCollection
 {
     public function toArray($request)
     {
-        $grouped = $this->collection->groupBy('category_id');
-
-        $categories = Category::whereIn('id', $grouped->keys())
-            ->get()
-            ->keyBy('id');
+        $grouped = $this->collection->groupBy(function ($favorite) {
+            return $favorite->product->store->category->id;
+        });
 
         return [
-            'categories' => $grouped->map(function ($favorites, $categoryId) use ($categories) {
+            'categories' => $grouped->map(function ($favorites) {
+                $category = $favorites->first()->product->store->category;
+
                 return [
-                    'category_name' => $categories[$categoryId]->name ?? null,
+                    'category_name' => $category->name,
                     'favorites' => FavoriteResource::collection($favorites),
                 ];
             })->values(),
