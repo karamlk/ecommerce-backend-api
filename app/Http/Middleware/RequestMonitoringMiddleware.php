6dd874650990e278d6260f00472b30d1af2824a1
karamlk <karam.alrifai24@gmail.com>
2026-05-03 21:31:33 +0300

    feat(monitoring): log request duration and route information

--- app/Http/Middleware/RequestMonitoringMiddleware.php
+++ app/Http/Middleware/RequestMonitoringMiddleware.php
@@ -5,21 +5,32 @@ namespace App\Http\Middleware;
 use App\Aspects\TracingAspect;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class RequestMonitoringMiddleware
 {
-     public function __construct(private TracingAspect $tracing) {}
+    public function __construct(private TracingAspect $tracing) {}
 
     public function handle(Request $request, Closure $next): Response
     {
+        $start   = microtime(true);
         $traceId = $this->tracing->startTrace();
 
         $response = $next($request);
 
-        $response->headers->set('X-Trace-Id', $traceId);
+        $duration = round((microtime(true) - $start) * 1000, 2);
 
-        $this->tracing->endTrace($response->getStatusCode());
+        Log::channel('tracing')->info('[TRACE] Request complete', [
+            'trace_id'    => $traceId,
+            'method'      => $request->method(),
+            'url'         => $request->path(),
+            'status_code' => $response->getStatusCode(),
+            'duration_ms' => $duration,
+        ]);
+
+        $response->headers->set('X-Trace-Id', $traceId);
+        $response->headers->set('X-Duration-Ms', $duration);
 
         return $response;
     }
